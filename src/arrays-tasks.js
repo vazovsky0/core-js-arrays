4e129df599c7ad2f7653d70d98c16de6f8056a3a
Zaur Gurbanov <crypton.qurbanov.1991@gmail.com>
2025-11-08 17:11:41 +0400

    feat: add solution of task isValueEqualsIndex

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -181,8 +181,11 @@ function isSameLength(arr) {
  *    isValueEqualsIndex([2, 1, 0, 4, 5]) => true
  *    isValueEqualsIndex([10, 20, 30, 40, 50]) => false
  */
-function isValueEqualsIndex(/* arr */) {
-  throw new Error('Not implemented');
+function isValueEqualsIndex(arr) {
+  if (!Array.isArray(arr)) {
+    throw new TypeError("Input must be an array");
+  }
+  return arr.some((value, index) => value === index);
 }
 
 /**
