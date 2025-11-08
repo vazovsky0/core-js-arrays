d2da7a82fddcc2b066ad969b4d4a1bc2b43da4d2
Zaur Gurbanov <crypton.qurbanov.1991@gmail.com>
2025-11-08 16:58:07 +0400

    feat: add solution of task removeFalsyValues

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -105,9 +105,13 @@ function findAllOccurrences(arr, item) {
  *    removeFalsyValues([ 1, 2, 3, 4, 5, 'false' ]) => [ 1, 2, 3, 4, 5, 'false' ]
  *    removeFalsyValues([ false, 0, NaN, '', undefined ]) => [ ]
  */
-function removeFalsyValues(/* arr */) {
-  throw new Error('Not implemented');
+function removeFalsyValues(arr) {
+  if (!Array.isArray(arr)) {
+    throw new TypeError("Input must be an array");
+  }
+  return arr.filter(Boolean);
 }
+
 
 /**
  * Returns an array containing the lengths of each string in a specified array of strings.
